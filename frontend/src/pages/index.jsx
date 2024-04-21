bd94504161f82b30b90a3f9f923177fd345911b6
rgligora <99548015+rgligora@users.noreply.github.com>
2024-04-21 16:11:16 +0200

    Added pages templates

---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,5 @@
+export { default as Line } from './Charts/Line';
+export { default as Pie } from './Charts/Pie';
+export { default as Devices } from './Devices';
+export { default as Software } from './Software';
+export { default as SoftwarePackages } from './SoftwarePackages';
