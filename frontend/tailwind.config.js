4b29d36a4f01a5d49657fbe7c360ecdca2115fed
rgligora <99548015+rgligora@users.noreply.github.com>
2024-04-27 15:58:15 +0200

    Fix config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -38,10 +38,6 @@ module.exports = {
         minHeight: {
           590: '590px',
         },
-        backgroundImage: {
-          'hero-pattern':
-            "url('https://i.ibb.co/MkvLDfb/Rectangle-4389.png')",
-        },
       },
     },
     plugins: [],
