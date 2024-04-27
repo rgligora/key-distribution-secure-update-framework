5950e926ad992d34bffd5342369a9e3060244253
rgligora <99548015+rgligora@users.noreply.github.com>
2024-04-27 11:56:08 +0200

    Added State Context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {FiSettings} from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-
+import {Button, ChartsHeader, Footer, Header, Navbar, Sidebar, ThemeSettings, UserProfile, LineChart, PieChart} from './components';
+import {Dashboard, Devices, Software, SoftwarePackages, Line, Pie} from './pages'
 import './App.css';
 
+import { useStateContext } from './contexts/ContextProvider';
+
 const App = () => {
-    const activeMenu = true;
+    const {activeMenu} = useStateContext();
+
     return (
     <div>
         <BrowserRouter>
@@ -25,30 +29,30 @@ const App = () => {
             </div>
             {activeMenu ? 
                 (<div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-                    Sidebar
+                    <Sidebar />
                 </div>) : 
                 (<div className='w-0 dark:bg-secondary-dark-bg'>
-                    
+                    <Sidebar />
                 </div>)}
             <div className={activeMenu ? 'dark:bg-main-bg bg-main-bg min-h-screen w-full md:ml-72' : 'dark:bg-main-bg bg-main-bg min-h-screen w-full flex-2'}>
                 <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-                    navbar
+                    <Navbar />
                 </div>
             </div>
             <div>
                 <Routes>
                     {/*Dashboard*/}
-                    <Route path='/' element="Company" />
-                    <Route path='/company' element="Company" />
+                    <Route path='/' element={<Dashboard />} />
+                    <Route path='/company' element={<Dashboard />} />
 
                     {/*Pages*/}
-                    <Route path='/devices' element="Devices"/>
-                    <Route path='/software' element="Software"/>
-                    <Route path='/software-packages' element="Software Packages"/>
+                    <Route path='/devices' element={<Devices />} />
+                    <Route path='/software' element={<Software />} />
+                    <Route path='/software-packages' element={<SoftwarePackages />} />
 
                     {/*Charts*/}
-                    <Route path='/line' element="Line"/>
-                    <Route path='/pie' element="Pie"/>
+                    <Route path='/line' element={<Line />} />
+                    <Route path='/pie' element={<Pie />} />
                     
                 </Routes>
             </div>
