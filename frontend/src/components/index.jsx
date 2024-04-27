be247cc18caf298925863e48e73c02b594c0307f
rgligora <99548015+rgligora@users.noreply.github.com>
2024-04-27 12:05:48 +0200

    Fix

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -3,7 +3,6 @@ export { default as ThemeSettings } from './ThemeSettings';
 export { default as Sidebar } from './Sidebar';
 export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
-export { default as Notification } from './Notification';
 export { default as UserProfile } from './UserProfile';;
 export { default as LineChart } from './Charts/LineChart';
 export { default as Pie } from './Charts/PieChart';
