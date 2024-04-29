2990f2931c75d92affc728ed05e40694df7ca3dd
rgligora <99548015+rgligora@users.noreply.github.com>
2024-04-29 20:17:59 +0200

    Added retrieveDevices

--- backend/src/main/java/hr/fer/kdsuf/controller/DeviceController.java
+++ backend/src/main/java/hr/fer/kdsuf/controller/DeviceController.java
@@ -1,6 +1,7 @@
 package hr.fer.kdsuf.controller;
 
 import hr.fer.kdsuf.model.dto.DeviceDto;
+import hr.fer.kdsuf.model.dto.UpdateHistoryDto;
 import hr.fer.kdsuf.model.request.CreateDeviceRequest;
 import hr.fer.kdsuf.repository.DeviceRepository;
 import hr.fer.kdsuf.service.Impl.DeviceServiceImpl;
@@ -8,6 +9,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/devices")
@@ -28,6 +31,13 @@ public class DeviceController {
     @GetMapping("/{id}")
     public ResponseEntity<DeviceDto> retrieveDevice(@PathVariable("id") String id){
         return ResponseEntity.ok(service.retrieveDevice(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DeviceDto>> retrieveDevices(
+            @RequestParam(required = false) String companyId
+    ){
+        return ResponseEntity.ok(service.retrieveDevices(companyId));
     }
 
     @DeleteMapping("/{id}")
