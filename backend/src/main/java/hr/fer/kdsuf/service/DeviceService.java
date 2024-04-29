291f30e91a75a2bdd3986fbf709f937cc1432ddc
rgligora <99548015+rgligora@users.noreply.github.com>
2024-04-29 20:18:37 +0200

    Implemented logic for retieveDevices

--- backend/src/main/java/hr/fer/kdsuf/service/DeviceService.java
+++ backend/src/main/java/hr/fer/kdsuf/service/DeviceService.java
@@ -3,10 +3,14 @@ package hr.fer.kdsuf.service;
 import hr.fer.kdsuf.model.dto.DeviceDto;
 import hr.fer.kdsuf.model.request.CreateDeviceRequest;
 
+import java.util.List;
+
 public interface DeviceService {
     DeviceDto createDevice(CreateDeviceRequest request);
 
     DeviceDto retrieveDevice(String id);
+
+    List<DeviceDto> retrieveDevices(String companyId);
 
     void deleteDevice(String id);
 }
--- backend/src/main/java/hr/fer/kdsuf/service/Impl/DeviceServiceImpl.java
+++ backend/src/main/java/hr/fer/kdsuf/service/Impl/DeviceServiceImpl.java
@@ -4,6 +4,7 @@ import hr.fer.kdsuf.exception.exceptions.DeviceNotFoundException;
 import hr.fer.kdsuf.mapper.DeviceMapper;
 import hr.fer.kdsuf.model.domain.Company;
 import hr.fer.kdsuf.model.domain.Device;
+import hr.fer.kdsuf.model.domain.UpdateHistory;
 import hr.fer.kdsuf.model.dto.DeviceDto;
 import hr.fer.kdsuf.model.request.CreateDeviceRequest;
 import hr.fer.kdsuf.repository.CompanyRepository;
@@ -11,6 +12,8 @@ import hr.fer.kdsuf.repository.DeviceRepository;
 import hr.fer.kdsuf.service.DeviceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DeviceServiceImpl implements DeviceService {
@@ -41,6 +44,17 @@ public class DeviceServiceImpl implements DeviceService {
     public DeviceDto retrieveDevice(String id) {
         Device device = deviceRepository.findById(id).orElseThrow(() -> new DeviceNotFoundException(id));
         return deviceMapper.modelToDto(device);
+    }
+
+    @Override
+    public List<DeviceDto> retrieveDevices(String companyId) {
+        List<Device> devices;
+        if(companyId != null){
+            devices = deviceRepository.findDevicesByCompanyCompanyId(companyId);
+        }else{
+            devices = deviceRepository.findAll();
+        }
+        return deviceMapper.modelToDtos(devices);
     }
 
     @Override
