426a4b717c0ab8768d9c925926c6498dc0d8254a
rgligora <99548015+rgligora@users.noreply.github.com>
2024-05-08 19:20:49 +0200

    Added ModelDto

---
+++ backend/src/main/java/hr/fer/kdsuf/model/dto/ModelDto.java
@@ -0,0 +1,24 @@
+package hr.fer.kdsuf.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ModelDto {
+
+    private String modelId;
+
+    private String name;
+
+    private String companyId;
+
+    private List<String> deviceIds;
+
+}
