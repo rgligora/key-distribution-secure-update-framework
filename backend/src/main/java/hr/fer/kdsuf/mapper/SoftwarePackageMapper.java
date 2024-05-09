ba92bd14323856ca31d0cacde8acb3b7ec9e3319
rgligora <99548015+rgligora@users.noreply.github.com>
2024-05-10 01:09:09 +0200

    Refactor SoftwarePackageMapper

--- backend/src/main/java/hr/fer/kdsuf/mapper/SoftwarePackageMapper.java
+++ backend/src/main/java/hr/fer/kdsuf/mapper/SoftwarePackageMapper.java
@@ -1,5 +1,6 @@
 package hr.fer.kdsuf.mapper;
 
+import hr.fer.kdsuf.model.domain.Model;
 import hr.fer.kdsuf.model.domain.SoftwarePackage;
 import hr.fer.kdsuf.model.dto.SoftwarePackageDto;
 import hr.fer.kdsuf.model.request.CreateSoftwarePackageRequest;
@@ -7,6 +8,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "Spring")
 public interface SoftwarePackageMapper {
@@ -15,8 +17,14 @@ public interface SoftwarePackageMapper {
     SoftwarePackage requestToModel(CreateSoftwarePackageRequest request);
 
     @Mapping(target = "companyId", source = "company.companyId")
+    @Mapping(target = "modelIds", expression = "java(packageToModelIds(softwarePackage))")
     SoftwarePackageDto modelToDto(SoftwarePackage softwarePackage);
 
     List<SoftwarePackageDto> modelToDtos(List<SoftwarePackage> softwarePackages);
 
+    default List<String> packageToModelIds(SoftwarePackage softwarePackage) {
+        return softwarePackage.getModels() != null ? softwarePackage.getModels().stream()
+                .map(Model::getModelId)
+                .collect(Collectors.toList()) : null;
+    }
 }
