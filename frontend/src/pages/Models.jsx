63dac251c413ba1f43e1cf05e2e685c1bee5c521
rgligora <99548015+rgligora@users.noreply.github.com>
2024-05-13 16:14:04 +0200

    Models serialNos window

--- frontend/src/pages/Models.jsx
+++ frontend/src/pages/Models.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, Edit, Inject } from '@syncfusion/ej2-react-grids';
-import { Header, Button, Modal, NewModelForm } from '../components';
-import { fetchData, createData, fetchDataWithRequestParams } from '../api.js';
-
+import { Header, Button, Modal } from '../components';
+import { fetchDataWithRequestParams } from '../api.js';
 
 const Models = ({ companyId }) => {
   const [ModelData, setModelData] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedModel, setSelectedModel] = useState(null);
+  const [isDetailModalOpen, setIsDetailModalOpen] = useState(false);
 
   const fetchModels = async () => {
     try {
       const data = await fetchDataWithRequestParams('models', { companyId });
+      console.log(data)
       setModelData(data);
     } catch (error) {
       console.error('Failed to load Models:', error);
@@ -21,15 +23,13 @@ const Models = ({ companyId }) => {
     fetchModels();
   }, [companyId]);
 
-  const handleNewModelsubmit = async (formData) => {
-    try {
-      const createdModel = await createData('models', { ...formData, companyId });
-      setModelData([...ModelData, createdModel]);
-    } catch (error) {
-      console.error('Failed to create new Model:', error);
-    }
-  };
+  const handleRowSelected = (args) => {
+    const selectedData = args.data;
+    const serialNumbers = selectedData.serialNos.map(serialNo => ({ serialNo }));
+    setSelectedModel({ ...selectedData, serialNos: serialNumbers });
+    setIsDetailModalOpen(true);
 
+  };
 
   const ModelsGrid = [
     { field: 'modelId', headerText: 'Model ID', width: 'auto', textAlign: 'Center' },
@@ -39,18 +39,20 @@ const Models = ({ companyId }) => {
   return (
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category="Page" title="Models" />
-      <Button
-        className="mb-5 p-3 bg-teal-600 text-white rounded-lg hover:bg-teal-700"
-        onClick={() => setIsModalOpen(true)}
-      >
+      <Button className="mb-5 p-3 bg-teal-600 text-white rounded-lg hover:bg-teal-700" onClick={() => setIsModalOpen(true)}>
         Add New Model
       </Button>
-
       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} title="Add New Model">
-        <NewModelForm onSubmit={handleNewModelsubmit} onClose={() => setIsModalOpen(false)} />
       </Modal>
-
-      <GridComponent id='gridcomp' dataSource={ModelData} allowPaging allowSorting>
+      <Modal isOpen={isDetailModalOpen} onClose={() => setIsDetailModalOpen(false)} title={`Serial Numbers for: ${selectedModel?.name}`}>
+        <GridComponent dataSource={selectedModel?.serialNos} allowPaging={true} pageSettings={{ pageSize: 10 }}>
+          <ColumnsDirective>
+            <ColumnDirective field="serialNo" headerText="Serial Number" textAlign="Center" />
+          </ColumnsDirective>
+          <Inject services={[Page]} />
+        </GridComponent>
+      </Modal>
+      <GridComponent id='gridcomp' dataSource={ModelData} allowPaging allowSorting rowSelected={handleRowSelected}>
         <ColumnsDirective>
           {ModelsGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
