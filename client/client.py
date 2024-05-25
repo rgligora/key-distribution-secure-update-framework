614f98bf31d5040db10b4c25e9927779aad5e4cc
rgligora <99548015+rgligora@users.noreply.github.com>
2024-05-25 13:20:25 +0200

    Load predicting

--- client/client.py
+++ client/client.py
@@ -8,7 +8,10 @@ ca_cert = "path/to/CA_cert.pem"
 SERIAL_NO = "7c3ef80b-9ea0-4d67-b494-67a18bb22a27"
 
 LOAD_HISTORY_WINDOW = 30
-SNAPSHOT_INTERVAL = 1
+PREDICT_WINDOW = 10
+SNAPSHOT_INTERVAL = 0.2
+CPU_THRESHOLD = 30.0
+MEMORY_THRESHOLD = 80.0
 
 cpuLoadHistory = deque(maxlen=LOAD_HISTORY_WINDOW)
 memoryLoadHistory = deque(maxlen=LOAD_HISTORY_WINDOW)
@@ -16,9 +19,22 @@ memoryLoadHistory = deque(maxlen=LOAD_HISTORY_WINDOW)
 def systemLoadSnaphot():
     cpuUsage = psutil.cpu_percent(interval=1)
     memoryUsage = psutil.virtual_memory().percent
-    print(f"CPU Usage: {cpuUsage}%, Memory Usage: {memoryUsage}%")
+    print(f"CPU usage: {cpuUsage}, MEMORY usage: {memoryUsage}")
     cpuLoadHistory.append(cpuUsage)
     memoryLoadHistory.append(memoryUsage)
+
+def isOptimalSystemLoad(cpuThreshold=CPU_THRESHOLD, memoryThreshold=MEMORY_THRESHOLD):
+    predictedCpuUsage, predictedMemoryUsage = predictSystemLoad()
+    print(f"Predicted CPU Usage: {predictedCpuUsage}%, Predicted Memory Usage: {predictedMemoryUsage}%")
+    return predictedCpuUsage < cpuThreshold and predictedMemoryUsage < memoryThreshold
+
+def predictSystemLoad(predictWindow=PREDICT_WINDOW):
+    cpuLoadList = list(cpuLoadHistory)
+    memoryLoadList = list(memoryLoadHistory)
+    predictedCpuUsage = sum(cpuLoadList[-predictWindow:]) / len(cpuLoadList[-predictWindow:])
+    predictedMemoryUsage = sum(memoryLoadList[-predictWindow:]) / len(memoryLoadList[-predictWindow:])
+    return predictedCpuUsage, predictedMemoryUsage
+
 
 
 def register_device():
@@ -46,12 +62,13 @@ def main():
 
     while True:
         systemLoadSnaphot()
-        print(cpuLoadHistory)
-        print(memoryLoadHistory)
+
+        if isOptimalSystemLoad():
+            print("INIT")
+        else:
+            print("Waiting for optimal sytem load")
         time.sleep(SNAPSHOT_INTERVAL)
 
-
-        
 
 if __name__ == "__main__":
     main()
