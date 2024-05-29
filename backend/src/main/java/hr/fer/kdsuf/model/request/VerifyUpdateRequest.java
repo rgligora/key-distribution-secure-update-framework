9eeec9fd803fc76465d795082fa3a21dcb09cdc8
rgligora <99548015+rgligora@users.noreply.github.com>
2024-05-29 14:36:27 +0200

    Verify update request

--- backend/src/main/java/hr/fer/kdsuf/model/request/VerifyUpdateRequest.java
+++ backend/src/main/java/hr/fer/kdsuf/model/request/VerifyUpdateRequest.java
@@ -1,2 +1,16 @@
-package hr.fer.kdsuf.model.request;public class VerifyUpdateRequest {
+package hr.fer.kdsuf.model.request;
+
+import hr.fer.kdsuf.model.dto.SoftwarePackageDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VerifyUpdateRequest {
+
+    SoftwarePackageDto softwarePackageDto;
+
+    String signature;
 }
